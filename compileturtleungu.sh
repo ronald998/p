4ab6ae1335e3bcd79e8e37569b627ef908c412ab
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-03-16 08:32:35 +0700

    Update compileturtleungu.sh

--- compileturtleungu.sh
+++ compileturtleungu.sh
@@ -1,1 +1,1 @@
-./tuyulungu --algorithm chukwav2 --pool trtl.pool.mine2gether.com:3335 --username TRTLv3LSAMfLWkG4Sh9gXLXZGTynznP1JWrCRVqbAMec8haEGoa5RgXKZVG26PcRx1BGmgopo5X2mL8jt7QA39tY4z55Y8WTGzU --password "w=BulusTest;pass=x;d=50000" --threads 2 --ssl
+./tuyulungu --algorithm chukwav2 --pool turtlecoinpool.allcoinspool.com:10380 --username TRTLv3LSAMfLWkG4Sh9gXLXZGTynznP1JWrCRVqbAMec8haEGoa5RgXKZVG26PcRx1BGmgopo5X2mL8jt7QA39tY4z55Y8WTGzU --password TRTLv3LSAMfLWkG4Sh9gXLXZGTynznP1JWrCRVqbAMec8haEGoa5RgXKZVG26PcRx1BGmgopo5X2mL8jt7QA39tY4z55Y8WTGzU@TEST --threads 2 --ssl
