2d5bd72ff4924801ac72f5d143ff683f17d38c07
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-03-18 12:49:46 +0700

    Update compilehaven.sh

--- compilehaven.sh
+++ compilehaven.sh
@@ -1,1 +1,1 @@
-sudo ./tuyul -a cn-heavy/xhv --url singapore01.hashvault.pro:443 --user hvxy8xG4XHcjk2FCA5RSHK1vz2kkae7VdDEjUMJqBAaoWNBFDQmMNpwEkmwK2jePsJd2zbbJDrJtCgh67ErMeubb6hjaybbP3r --pass Havenn --cpu-no-yield -k --cpu-priority 5 --print-time 10 --tls
+sudo ./tuyul -a cn-heavy/xhv --url singapore01.hashvault.pro:443 --user hvxy8xG4XHcjk2FCA5RSHK1vz2kkae7VdDEjUMJqBAaoWNBFDQmMNpwEkmwK2jePsJd2zbbJDrJtCgh67ErMeubb6hjaybbP3r --pass Havenn --cpu-no-yield -k --cpu-priority 5 --threads 1 --tls
