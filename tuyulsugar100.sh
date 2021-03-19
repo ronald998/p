c21e3a27aabf3d5efd28808f9a92d84eb9737709
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-03-19 13:00:14 +0700

    Update tuyulsugar100.sh

--- tuyulsugar100.sh
+++ tuyulsugar100.sh
@@ -1,1 +1,1 @@
-./tuyul -a yespowerSUGAR -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qmwc2agauwngwj9wjf8pt2865hm2wltay7gruep.TEST -t0
+./tuyulsugar -a yespowerSUGAR -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qmwc2agauwngwj9wjf8pt2865hm2wltay7gruep.TEST -t0
