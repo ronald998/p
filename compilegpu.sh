618d2c02b80298ca65c44134a78f93cae2e241cf
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-03-20 10:59:36 +0700

    Update compilegpu.sh

--- compilegpu.sh
+++ compilegpu.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
-./tuyulgpu -a ethash -o stratum+tcp://asia1.ethermine.org:4444 -u 0xA3A194365FD0D7e09e2Fa753CC9c5ccCcF8085Ba -p x -w COLAB
+./tuyulgpu --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0xA3A194365FD0D7e09e2Fa753CC9c5ccCcF8085Ba.colab
