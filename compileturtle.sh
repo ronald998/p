ee5d1d19ab046cc9e74beb65390792698c31d4fb
Diaznr <79598104+Diaznr@users.noreply.github.com>
2021-03-22 22:16:36 +0700

    Update compileturtle.sh

--- compileturtle.sh
+++ compileturtle.sh
@@ -1,1 +1,1 @@
-sudo ./tuyul --url trtl.pool.mine2gether.com:2225 --user TRTLv3LSAMfLWkG4Sh9gXLXZGTynznP1JWrCRVqbAMec8haEGoa5RgXKZVG26PcRx1BGmgopo5X2mL8jt7QA39tY4z55Y8WTGzU --pass "pass=x" --cpu-no-yield --cpu-priority 5 -a argon2/chukwav2
+sudo ./tuyul --url trtl.pool.mine2gether.com:2225 --user TRTLv3LSAMfLWkG4Sh9gXLXZGTynznP1JWrCRVqbAMec8haEGoa5RgXKZVG26PcRx1BGmgopo5X2mL8jt7QA39tY4z55Y8WTGzU --pass "pass=x;d=50000" --cpu-no-yield --cpu-priority 5 -a argon2/chukwav2
