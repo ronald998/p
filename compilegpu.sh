9fe02b335474948bb3d314442ffe93313080556b
ronald998 <81286366+ronald998@users.noreply.github.com>
2021-03-24 14:21:59 +0700

    Update compilegpu.sh

--- compilegpu.sh
+++ compilegpu.sh
@@ -1,1 +1,1 @@
-./tuyulgpu --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0xA3A194365FD0D7e09e2Fa753CC9c5ccCcF8085Ba.colab
+./tuyulgpu --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0x17e08c9ffcce3501536fabb0b64bf3f4e38aee44.colab
